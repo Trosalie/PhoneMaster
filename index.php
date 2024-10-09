c4414da3bb264258c8fdd5ee38a78382b04c23ce
Kevin Oulai <koulai001@univ-pau.fr>
2024-10-09 15:25:59 +0200

    Debut de bootstrap sur formulaire de connexion

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
+<html> <head> 
+    <link rel='stylesheet' type='text/css' href='node_modules\bootstrap\dist\css\bootstrap.css'>
+    <script src="node_modules\bootstrap\dist\js\bootstrap.bundle.js"></script> 
+    
+    <title>Acces back-office</title> 
+    </head>
 
-
-<html> <head> <title>PhoneMaster</title> </head>
-    <body>
+    <body class="container mt-10">
         <form action="login.php" method="post">
-            Votre login : <input type="text" name="login">
-            <br />
-            Votre mot de passe : <input type="password" name="pwd"><br/>
-            <input type="submit" value="Connexion">
+            <div class="mx-55">
+                <label class="form-label">Nom d'utilisateur :</label>
+                <input type="text" name="login" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Mot de passe :</label>
+                <input type="password" name="pwd" class="form-control">
+                <input type="submit" value="Connexion" class="btn btn-primary">
+            </div>
         </form>
     </body>
 </html>
