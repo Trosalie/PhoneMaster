56f8d6952bceb54862952cb994a7390ec5f32f1a
Kevin Oulai <koulai001@univ-pau.fr>
2024-10-12 14:06:04 +0200

    Upload de photo lors d'un ajout

--- ajoutBD.php
+++ ajoutBD.php
@@ -19,13 +19,15 @@
                         <div class="card-body p-5 text-center">
                             <label class="fs-2">Ajouter un enregistrement</label>
                             <br/><br/>
-                            <form action="ajoutBD.php" method="POST">
+                            <form ENCTYPE='multipart/form-data' action="upload.php" method="POST">
                             <div class="input-group mb-3">
                                 <input class="input-group-text" type=text name=id placeholder=id>
                                 <input class="input-group-text" type=text name=modele placeholder=modele>
                                 <input class="input-group-text" type=text name=marque placeholder=marque>
                                 <input class="input-group-text" type=text name=prix placeholder=prix>
                             </div>
+                                <input type=file name=photo accept=".jpg,.jpeg,.png" class="form-control">
+                                <br>
                                 <input type=submit value=Ajouter class="btn btn-primary">
                             </form>
                             <button onclick="window.location.href='backoffice.php';" class="btn text-bg-danger" >Retour</button>
@@ -34,42 +36,5 @@
                 </div>
             </div>
         </div>
-<?php
-    $bdd = "koulai001_bd"; // Base de données 
-    $host = "lakartxela.iutbayonne.univ-pau.fr";
-    $user = "koulai001_bd"; // Utilisateur 
-    $pass = "koulai001_bd"; // mp
-    $nomtable = "Telephone"; /* Connection bdd */ 
-    $link = mysqli_connect($host,$user,$pass,$bdd) or die( "Impossible de se connecter à la base de données<br>");
-
-    $query = "SELECT id FROM $nomtable";
-    $resultat = mysqli_query($link,$query);
-    $i = 1;
-
-    if(isset($_POST["id"])){
-        $id = $_POST["id"];
-        $modele = $_POST["modele"];
-        $marque = $_POST["marque"];
-        $prix = $_POST["prix"];
-
-        while($donnee = mysqli_fetch_assoc($resultat)){
-            $idBD = $donnee["id"];
-            if($id == $idBD){
-                echo '<body onLoad="alert(\'ID existant dans la base de données\')">';
-                $resultat -> free_result();
-                $link -> close();
-                exit();
-            }
-        }
-
-        if($query = $link->prepare("INSERT INTO `Telephone` (id, modele, marque, prix) VALUE ((?),(?),(?),(?))"))
-        {
-            $query->bind_param('issd',$_POST["id"],$_POST["modele"],$_POST["marque"],$_POST["prix"]);
-            $query->execute();
-            echo '<meta http-equiv="refresh" content="0;URL=backoffice.php">';
-        }
-
-    }
-    $resultat -> free_result();
-    $link -> close();
-?>
+</main>
+</html>
--- upload.php
+++ upload.php
@@ -1,20 +1,48 @@
-<HTML><BODY>
-    <?php
-    $nombrePhoto = $_POST["nbphotos"];
 
-    if(!is_dir("upload")){
-        mkdir("upload",0200);
-    }
+<?php
 
-    $dir = "upload";
-    
-    for ($i=0; $i < $nombrePhoto; $i++) { 
-        if(is_uploaded_file($_FILES["photo$i"]["tmp_name"])){
-            $name = basename($_FILES["photo$i"]["name"]);
-            move_uploaded_file($_FILES["photo$i"]["tmp_name"],"$dir/$name");
-            print "L'image : $name, a été téléchargée dans le fichier $dir<br>";
+    $dir = "images"; // Nom du dossier contenant les photos
+
+    $name = $_POST["id"]."_".$_POST["modele"].".png"; // Nom du fichier de photo
+
+
+    $bdd = "koulai001_bd"; // Base de données
+    $host = "lakartxela.iutbayonne.univ-pau.fr";
+    $user = "koulai001_bd"; // Utilisateur
+    $pass = "koulai001_bd"; // mp
+    $nomtable = "Telephone"; /* Connection bdd */
+    $link = mysqli_connect($host,$user,$pass,$bdd) or die( "Impossible de se connecter à la base de données<br>");
+
+
+    $query = "SELECT id FROM $nomtable";
+    $resultat = mysqli_query($link,$query);
+    $i = 1;
+
+
+    $id = $_POST["id"];
+    $modele = $_POST["modele"];
+    $marque = $_POST["marque"];
+    $prix = $_POST["prix"];
+
+    while ($donnee = mysqli_fetch_assoc($resultat)) {
+        $idBD = $donnee["id"];
+        if ($id == $idBD) {
+            echo '<body onLoad="alert(\'ID existant dans la base de données\')">';
+            $resultat->free_result();
+            $link->close();
+            exit();
         }
     }
 
-    ?>
-</BODY></HTML>
+    if(is_uploaded_file($_FILES["photo"]["tmp_name"])){
+        //$name = basename($_FILES["photo"]["name"]);
+        move_uploaded_file($_FILES["photo"]["tmp_name"],"$dir/$name");
+        print '<meta http-equiv="refresh" content="0;URL=backoffice.php">';
+    }
+
+
+    if ($query = $link->prepare("INSERT INTO `Telephone` (id, modele, marque, prix, image) VALUE ((?),(?),(?),(?),(?))")) {
+        $query->bind_param('issds', $_POST["id"], $_POST["modele"], $_POST["marque"], $_POST["prix"], $name);
+        $query->execute();
+        echo '<meta http-equiv="refresh" content="0;URL=backoffice.php">';
+    }
